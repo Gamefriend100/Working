d17f10d093f538618ed76421425b66968e029209
Miguel Iran Reyes Uribe <124807939+Gamefriend100@users.noreply.github.com>
2026-02-24 22:55:54 -0600

    Refactor kit.controller.js to use React component

--- kit.controller.js
+++ kit.controller.js
@@ -1,32 +1,115 @@
-const db = require('../../database/models');
+import { Button, MUIAutocomplete, notifyError, notifySuccess, TextFieldError } from "../../../../components/shared";
+import { useGetCategorySupplies } from "../../../../hooks/safety/kit";
+import { UseAppContext } from "../../../../context/AppContext";
+import { TaskAlt } from "@mui/icons-material";
+import { useState } from "react";
+import { appApi } from "../../../../api";
+import { UseSuppliesContext } from "../../../../context/safety/kit/supplies/SuppliesContext";
 
-const getKit = async (req, res) => {
-  try {
-    const kits = await db.SafKit.findAll({
-      include: [
-        { model: db.SafSupplies, as: 'supply', attributes: ['name'] },
-        { model: db.Branch, as: 'branch', attributes: ['name'] },
-      ],
-    });
+const Form = ({ item }) => {
+  const { authUserToken } = UseAppContext();
+  const { setModalSupplies, supplies, setSupplies } = UseSuppliesContext();
 
-    const data = kits.map(kit => ({
-      value: kit.id,
-      label: `${kit.supply?.name || 'Sin supply'} - ${kit.branch?.name || 'Sin branch'}`,
-      quantity: kit.quantity,
-      supplyName: kit.supply?.name || null,
-      branchName: kit.branch?.name || null,
-    }));
+  const [identifier, setIdentifier] = useState(item?.identifier || "");
+  const [name, setName] = useState(item?.name || "");
+  const [specification, setSpecification] = useState(item?.specification || "");
+  const [category, setCategory] = useState(item?.categoryId || null);
 
-    res.status(200).json(data);
-  } catch (error) {
-    console.error('Error al obtener los kits:', error);
-    res.status(500).json({
-      msg: 'Error al obtener los kits',
-      error: error.message,
-    });
-  }
+  const { data, isFetching } = useGetCategorySupplies(authUserToken).categories;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const method = item ? "put" : "post";
+      const url = item
+        ? `/intranet/supplies/edit/${item.id}`
+        : "/intranet/supplies/create";
+
+      const newData = {
+        identifier,
+        name,
+        specification,
+        categoryId: category,
+      };
+
+      const { data } = await appApi[method](url, newData, {
+        headers: {
+          Authorization: `Bearer ${authUserToken}`,
+        },
+      });
+      notifySuccess(data.msg)
+
+      const newLocalData = item 
+      ? supplies.map((s) =>s.id === item.id ? data.supply : s) 
+      : [data.supply, ...supplies];
+
+      setSupplies(newLocalData);
+
+    } catch (error) {
+      notifyError(error?.response?.data?.msg || "Algo salio mal, intenta de nuevo")
+    } finally {
+      setModalSupplies({ show: false });
+    }
+  };
+
+  return (
+    <div className="p-8 space-y-6">
+      <p className="uppercase font-semibold text-xl border-b pb-1">
+        {item ? "Editar" : "Agregar"} insumo
+      </p>
+
+      <form className="space-y-4" onSubmit={handleSubmit}>
+        <div className="grid grid-cols-3 gap-4">
+          <TextFieldError
+            required
+            name="identifier"
+            label="Identificador"
+            value={identifier}
+            onChange={(e) => {
+              setIdentifier(e.target.value);
+            }}
+          />
+
+          <div className="col-span-2">
+            <TextFieldError
+              required
+              name="name"
+              label="Nombre"
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
+          </div>
+        </div>
+
+        <TextFieldError
+          required
+          name="specification"
+          label="Presentación"
+          value={specification}
+          onChange={(e) => setSpecification(e.target.value)}
+        />
+
+        {!isFetching && (
+          <MUIAutocomplete
+            required
+            title={"Categoría"}
+            options={data}
+            value={category}
+            handleChange={({ value }) => setCategory(value)}
+          />
+        )}
+
+        <Button
+          text="Guardar"
+          icon={<TaskAlt fontSize="small" />}
+          type="submit"
+        />
+      </form>
+    </div>
+  );
 };
 
-module.exports = { 
-  getKit
- };
+export default Form;

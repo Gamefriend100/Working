50b373c111429608db9662629d9f47fa9a0df47a
Miguel Iran Reyes Uribe <124807939+Gamefriend100@users.noreply.github.com>
2026-03-02 22:35:03 -0600

    Implement KitContext with data fetching hooks
    Refactor kit.routes.js to use React context for managing kit data.

--- kit.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const router = Router();
-
-const { getKit } = require('../../controllers/safety/kit.controller');
-
-// GET todos los kits
-router.get('/', getKit);
-
-module.exports = router;
